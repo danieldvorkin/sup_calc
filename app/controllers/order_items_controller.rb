aa40f4f41a961a23f501d2d2c3bff6715e88af90
Daniel Dvorkin <danieldvorkin@Daniels-MacBook-Pro.local>
2017-11-30 16:58:32 -0500

    Fixed a small bug

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -36,7 +36,7 @@ class OrderItemsController < ApplicationController
   def destroy
     @order = current_order
     @order_item = @order.order_items.find(params[:id])
-    @product_id = @order_item.product.id
+    @product_id = @order_item.product.dataId
     @order_item.destroy
     @order_items = @order.order_items
 
