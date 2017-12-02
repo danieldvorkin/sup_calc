1aa0802f326a72b067a4928c8083679bb2322d11
Daniel Dvorkin <danieldvorkin@Daniels-MacBook-Pro.local>
2017-12-02 04:42:11 -0500

    more small changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,6 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.serve_static_assets = true
-  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
   config.assets.compile = true
 
   config.action_mailer.delivery_method = :sendmail
