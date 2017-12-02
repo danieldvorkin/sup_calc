b4e74a5a944806b543f064d2852d11d1564f60fa
Daniel Dvorkin <danieldvorkin@Daniels-MacBook-Pro.local>
2017-12-02 15:09:53 -0500

    new styles not coming through

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.serve_static_assets = true
+  config.serve_static_assets = false
   # config.assets.compile = true
 
   config.action_mailer.delivery_method = :sendmail
