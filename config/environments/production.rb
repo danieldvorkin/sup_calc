2100a89dd945d79aef6b4821bfaeef28c89b8ace
Daniel Dvorkin <danieldvorkin@Daniels-MacBook-Pro.local>
2017-12-02 04:39:58 -0500

    adjusted some prod settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,9 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.serve_static_assets = true
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  config.assets.compile = true
 
   config.action_mailer.delivery_method = :sendmail
   # Defaults to:
