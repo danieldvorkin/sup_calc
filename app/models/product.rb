7a3d6b9809e9407c1aa00e6828637b7f476686bf
Daniel Dvorkin <danieldvorkin@Daniels-MacBook-Pro.local>
2017-12-05 22:45:10 -0500

    only active products should be shown

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,7 +10,7 @@ class Product < ApplicationRecord
   end
   
   def self.specific_week(dropweek)
-    self.all.where(dropweek: dropweek)
+    self.all.where(dropweek: dropweek, active: true)
   end
   
   def self.filters
