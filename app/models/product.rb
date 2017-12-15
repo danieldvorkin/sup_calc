96180c767d05309c9601e2a32322fad8d48c19c3
Daniel Dvorkin <danieldvorkin@Daniels-MacBook-Pro.local>
2017-12-14 22:21:22 -0500

    general spacing fixes

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   has_many :order_items
 
   def label
-    "(USD): #{self.price.delete(' ').gsub("\n", "").split("/").shift(1)[0]}"
+    "#{self.price.delete(' ').gsub("\n", "").split("/").shift(1)[0]}"
   end
   
   def self.dropweeks
