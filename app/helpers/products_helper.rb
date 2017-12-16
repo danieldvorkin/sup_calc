e4457972d1014a7cc70ebf2cd44718d38d7dc93a
Daniel Dvorkin <danieldvorkin@Daniels-MacBook-Pro.local>
2017-12-16 17:19:28 -0500

    made a method to make the season label more human

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,7 +1,11 @@
 module ProductsHelper
 
   def self.get_season(week)
-    week.split("/").shift(3).last
+    newWeek = week.split("/").shift(3).last
+    fLetter = newWeek.split("-")[0].chars[0]
+    lLetter = newWeek.split("-")[1].split(/[^A-Za-z]/)[0].chars[0]
+    year = newWeek.split("-")[1].split(/[^0-9]/).reject(&:empty?)
+    "#{fLetter.capitalize}#{lLetter.capitalize}#{year[0]}"
   end
 
   def self.get_week(week)
